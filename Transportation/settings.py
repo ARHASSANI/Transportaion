e148119a7b88c0fc4168bb2f4b0cecef078f1b1e
Alireza Hassani <alirezahassani64@yahoo.com>
2023-02-21 12:33:30 +0330

    Update settings.py

--- Transportation/settings.py
+++ Transportation/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sz5p%h7mpthv&ulut@!%&b3tzzdy_=dxuye$x3=j+#5@mg*m27'
+#add your own secrect key
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
